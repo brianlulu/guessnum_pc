ca8b304327a13615a6feadd75a07a72b23c806ac
brianlulu <juiheng.lu@ryerson.ca>
2021-01-30 23:13:27 +0800

    Finish the project v1

--- main.py
+++ main.py
@@ -3,28 +3,62 @@ import random
 class GuessPc:
     
     def __init__(self):
-        self.guess = 0
         self.upper = int(input(f'Please enter an positive integer as upper bound of this guessing game: '))
+        self.lower = 1
+        self.guess = 0
         self.feedback = ''
+    
+    def guess_num(self, l, u):
+        self.guess = int((l+u)/2)
 
-    def guess(self):
-        return random.randint(1, self.upper)
+    def get_guess(self):
+        return self.guess
     
-    def set_upper(self, new_upper):
-        self.upper = new_upper
+    def set_upper(self, num):
+        self.upper = num
     
-    def new_upper_num(self, num):
-        if num % 2 == 0:
-            self.set_upper(num/2)
-        else:
-            self.set_upper((num+1)/2)
+    def set_lower(self, num):
+        self.lower = num
 
     def play(self):
+        
+        self.guess_num(self.lower, self.upper)
+
         while self.feedback != 'C':
-            self.guess = self.guess()
-            self.feedback = input(f'Is {self.guess} is Too High (H), Too Low(L), or Correct Answer(C)? ')
+
+            print(f"guess: {self.guess}, up:{self.upper}, low:{self.lower}")
+            
+            self.feedback = input(f'Is {self.get_guess()} is Too High (H), Too Low(L), or Correct Answer(C)?: ')
+
+            if self.upper - self.lower <= 1:
+                print("I think I got the right answer! Don't cheat!")
+                self.feedback = input(f'Is {self.get_guess()} is Too High (H), Too Low(L), or Correct Answer(C)?: ')
+            
+            old_guess = self.get_guess()
+            
             if self.feedback == 'H':
-                self.new_upper_num
-                self.guess() # fix this because its randint from 1 to upper bound
+                self.set_upper(old_guess)
+                self.guess_num(self.lower, self.upper)
+                
+
+            elif self.feedback == 'L':
+                self.set_lower(old_guess)
+                self.guess_num(self.lower, self.upper)
+            
+            elif self.feedback != 'C':
+                print(f'Invalid Value, Please Enter again!')
+                continue
+
+        print(f'I am a computer! This is way too easy for me! Try harder!')
+    
+
+if __name__ == '__main__':
+    
+    game1 = GuessPc()
+    print(game1.get_guess())
+    game1.play()
+
+
+
 
                 
